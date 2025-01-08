8a9ebd40d9602e97d51482d245a86fd40d6de0f3
mmd-yavarii <mdyavarii@gmail.com>
2025-01-08 12:39:43 +0330

    debug clock hands

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,6 +1,7 @@
 import { footerComponent, navComponent } from "./main.js";
 
 const digitalClockNode = document.querySelector("#digital-clock");
+const digitalUTCClockNode = document.querySelector("#digital-utc-clock");
 const hourHand = document.querySelector("#analog-hour");
 const minuteHand = document.querySelector("#analog-minut");
 const secondHand = document.querySelector("#analog-second");
@@ -8,7 +9,8 @@ const secondHand = document.querySelector("#analog-second");
 const digital = (time) => {
   const amOrPm = time.getHours() >= 12 ? "pm" : "am";
   const hour = time.getHours() % 12 || 12;
-  digitalClockNode.innerHTML = `${hour}:${time.getMinutes()}:${time.getSeconds()}-${amOrPm}`;
+  digitalClockNode.innerText = `${hour}:${time.getMinutes()}:${time.getSeconds()}-${amOrPm}`;
+  digitalUTCClockNode.innerText = `UTC / ${time.getUTCHours()}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
 };
 
 const analog = (time) => {
@@ -19,6 +21,13 @@ const analog = (time) => {
   const secondDegree = seconds * 6;
   const minuteDegree = minutes * 6 + seconds * 0.1;
   const hourDegree = hours * 30 + minutes * 0.5;
+
+  if (seconds === 0) {
+    secondHand.style.transition = "none";
+  } else {
+    secondHand.style.transition =
+      "transform 0.05s cubic-bezier(0.4, 2.3, 0.3, 1)";
+  }
 
   secondHand.style.transform = `translateX(-50%) rotate(${secondDegree}deg)`;
   minuteHand.style.transform = `translateX(-50%) rotate(${minuteDegree}deg)`;
